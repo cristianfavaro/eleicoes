b801641d85d9bb92ae5b90ddd4944338fb8050eb
crisfavaro <cristian.favaro@valor.com.br>
2023-11-16 19:42:36 -0300

    ajustes com o mapa funcionando no rewing

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,14 +1,71 @@
 import React, {useState} from 'react';
-import Map from './Components/Map';
 
 import GeoChart from './Components/Map/GeoChart';
 import "./App.css";
 
+import { select, geoPath, geoAlbersUsa, geoMercator, min, max, scaleLinear, json } from "d3";
 import data from "./Components/Map/data/GeoChart.world.geo.json";
+
+// import countyData from "./Components/Map/data/counties.json";
+// import stateData from "./Components/Map/data/states.json";
+import brasilData from "./Components/Map/data/brasil.json";
+
+const Map3 = () => {
+  const colors = ["#B9EDDD", "#87CBB9", "#569DAA", "#577D86"];
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const [data, setData] = useState(null);
+
+  // json("https://servicodados.ibge.gov.br/api/v3/malhas/paises/BR?formato=image/svg+xml&qualidade=minima&intrarregiao=UF").then(
+  //   response => setData(response)
+  // )
+
+  const colorScale = (data) => {
+    return colors[Math.floor(Math.random() * 4)]
+  };
+  
+  return <GeoChart 
+      data={brasilData} 
+      colorScale={colorScale}
+      onMouseOver={(d,i)=>{console.log(d, i)}}
+      selected={selectedCountry}
+      onClick={ (event, feature) =>setSelectedCountry(selectedCountry === feature ? null : feature)} 
+    />
+}
+
+// const Map2 = () => {
+//   const colors = ["#B9EDDD", "#87CBB9", "#569DAA", "#577D86"];
+//   const [selectedCountry, setSelectedCountry] = useState(null);
+
+//   const colorScale = (data) => {
+//     return colors[Math.floor(Math.random() * 4)]
+//   };
+
+//   return <GeoChart 
+//       data={countyData} 
+//       projection={geoAlbersUsa}
+//       colorScale={colorScale}
+//       selected={selectedCountry}
+//       onClick={ (event, feature) =>setSelectedCountry(selectedCountry === feature ? null : feature)} 
+//     />
+// }
+
 
 function App() {
 
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const property = "pop_est";
+
+  const minProp = min(data.features, feature => feature.properties[property]);
+  const maxProp = max(data.features, feature => feature.properties[property]);
+  
+  const scale = scaleLinear()
+      .domain([minProp, maxProp])
+      .range(["#ccc", "red"]);
+
+  const colorScale = (feature) => {
+    return scale(feature.properties[property])
+  } 
 
   return (
     <div className="App">
@@ -17,13 +74,15 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         
-        <Map></Map>
-        <GeoChart 
+        <Map3/>
+
+
+        {/* <GeoChart 
           data={data} 
+          colorScale={colorScale}
           selected={selectedCountry}
           onClick={ (event, feature) =>setSelectedCountry(selectedCountry === feature ? null : feature)} 
-          property={"pop_est"}
-        />
+        /> */}
 
       </header>
     </div>
--- app/frontend/src/Components/Map/GeoChart.js
+++ app/frontend/src/Components/Map/GeoChart.js
@@ -2,10 +2,12 @@ import React, { useRef, useEffect, useState } from "react";
 import { select, geoPath, geoMercator, min, max, scaleLinear } from "d3";
 
 /**
- * Component that renders a map of Germany.
+ * Component that renders a map.
+ * https://github.com/muratkemaldar/using-react-hooks-with-d3/blob/12-geo/src/GeoChart.js
  */
 
-function GeoChart({ data, property, selected, onClick}) {
+
+function GeoChart({ data, selected, onClick, colorScale, onMouseOver=(d,i)=>{}, projection=geoMercator}) {
   const svgRef = useRef();
   const wrapperRef = useRef();
   // const dimensions = useResizeObserver(wrapperRef);
@@ -14,24 +16,17 @@ function GeoChart({ data, property, selected, onClick}) {
   useEffect(() => {
     const svg = select(svgRef.current);
 
-    const minProp = min(data.features, feature => feature.properties[property]);
-    const maxProp = max(data.features, feature => feature.properties[property]);
-    const colorScale = scaleLinear()
-      .domain([minProp, maxProp])
-      .range(["#ccc", "red"]);
-
     // use resized dimensions
     // but fall back to getBoundingClientRect, if no dimensions yet.
     const { width, height } = wrapperRef.current.getBoundingClientRect();
-
     // projects geo-coordinates on a 2D plane
-    const projection = geoMercator()
+    const projec = projection()
       .fitSize([width, height], selected || data)
       .precision(100);
 
     // takes geojson data,
     // transforms that into the d attribute of a path element
-    const pathGenerator = geoPath().projection(projection);
+    const pathGenerator = geoPath().projection(projec);
 
     // render each country
     svg
@@ -39,27 +34,30 @@ function GeoChart({ data, property, selected, onClick}) {
       .data(data.features)
       .join("path")
       .on("click", onClick)
+      .on('mouseover', onMouseOver)
+      .on('mouseout', function (d, i){})
       .attr("class", "country")
       .transition()
-      .attr("fill", feature => colorScale(feature.properties[property]))
+      .attr("fill", feature => colorScale(feature))
       .attr("d", feature => pathGenerator(feature));
 
     // render text
-    svg
-      .selectAll(".label")
-      .data([selected])
-      .join("text")
-      .attr("class", "label")
-      .text(
-        feature =>
-          feature &&
-          feature.properties.name +
-            ": " +
-            feature.properties[property].toLocaleString()
-      )
-      .attr("x", 10)
-      .attr("y", 25);
-  }, [data, property, selected]);
+    // svg
+    //   .selectAll(".label")
+    //   .data([selected])
+    //   .join("text")
+    //   .attr("class", "label")
+    //   .text(
+    //     feature =>
+    //       feature &&
+    //       feature.properties.name +
+    //         ": " +
+    //         feature.properties[property].toLocaleString()
+    //   )
+    //   .attr("x", 10)
+    //   .attr("y", 25);
+
+  }, [data, selected]);
 
   return (
     <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
