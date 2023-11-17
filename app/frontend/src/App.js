0f115c2be2f0416cc3c56bda4069053441b75208
crisfavaro <cristian.favaro@valor.com.br>
2023-11-16 23:47:51 -0300

    ajustes com mapa funcionando

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -4,23 +4,29 @@ import GeoChart from './Components/Map/GeoChart';
 import "./App.css";
 
 import { select, geoPath, geoAlbersUsa, geoMercator, min, max, scaleLinear, json } from "d3";
-import data from "./Components/Map/data/GeoChart.world.geo.json";
 
 // import countyData from "./Components/Map/data/counties.json";
 // import stateData from "./Components/Map/data/states.json";
-import brasilData from "./Components/Map/data/brasil.json";
+import brasilData from "./Components/Map/data/br-estados.json";
+
+// for (let index = 0; index < brasilData["features"].length; index++) { 
+  
+//   brasilData["features"][index]["properties"] = {
+//     ...brasilData["features"][index]["properties"],
+//     "cd": estados[brasilData["features"][index]["properties"]["codarea"]]["sigla"],
+//   } 
+// }
 
 const Map3 = () => {
   const colors = ["#B9EDDD", "#87CBB9", "#569DAA", "#577D86"];
   const [selectedCountry, setSelectedCountry] = useState(null);
 
-  const [data, setData] = useState(null);
 
   // json("https://servicodados.ibge.gov.br/api/v3/malhas/paises/BR?formato=image/svg+xml&qualidade=minima&intrarregiao=UF").then(
   //   response => setData(response)
   // )
 
-  const colorScale = (data) => {
+  const colorScale = () => {
     return colors[Math.floor(Math.random() * 4)]
   };
   
@@ -56,16 +62,16 @@ function App() {
   const [selectedCountry, setSelectedCountry] = useState(null);
   const property = "pop_est";
 
-  const minProp = min(data.features, feature => feature.properties[property]);
-  const maxProp = max(data.features, feature => feature.properties[property]);
+  // const minProp = min(data.features, feature => feature.properties[property]);
+  // const maxProp = max(data.features, feature => feature.properties[property]);
   
-  const scale = scaleLinear()
-      .domain([minProp, maxProp])
-      .range(["#ccc", "red"]);
+  // const scale = scaleLinear()
+  //     .domain([minProp, maxProp])
+  //     .range(["#ccc", "red"]);
 
-  const colorScale = (feature) => {
-    return scale(feature.properties[property])
-  } 
+  // const colorScale = (feature) => {
+  //   return scale(feature.properties[property])
+  // } 
 
   return (
     <div className="App">
