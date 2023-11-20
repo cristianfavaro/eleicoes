014ad5807de3e846321c63821815b8cd7b998b67
crisfavaro <cristian.favaro@valor.com.br>
2023-11-20 01:40:02 -0300

    tudo fucnionando no pop

--- app/frontend/src/Components/PopOver/index.js
+++ app/frontend/src/Components/PopOver/index.js
@@ -3,38 +3,8 @@ import styled from 'styled-components';
 import useMousePosition from '../../hooks/useMousePosition';
 import {kFormatter} from '../../utils/kFormatter';
 import { colorPicker } from '../../utils/colorPicker';
+import { Container, BarContainer } from './styles';
 
-const Container = styled.div`
-  border: 1px solid darkgray;
-  background-color: white; 
-  width: 200px;
-  position: fixed;
-  transition: transform 0.23s; Sticking effect
-  pointer-events: none; /* Allow clicking trough the div */
-  border-radius: 5px;
-
-  .header{
-    background-color: lightblue;
-    font-size: small;
-  }
-  >div{
-    padding: 0.2rem;
-  }
-  
-  .cand{
-    display: flex;
-    justify-content: space-between;
-    small{
-      font-size: 0.6rem;
-    }
-    small:first-child{
-      width: 60%;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      overflow: hidden;
-    }
-  };
-`
 
 const Cand = ({cand}) => {
   const {e, n, nmu, p, pvap, seq, st, vap} = cand; 
@@ -44,14 +14,6 @@ const Cand = ({cand}) => {
   </div>
 }
 
-const BarContainer = styled.div`
-  height: 15px;
-  padding: 0.2rem 0;
-  display: flex;
-  .w3-grey{
-    background-color: gray;
-  }
-`
 
 const Bar =  React.memo(({cands}) => {
 
